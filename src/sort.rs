6c73918eec8488c27609e08854de7248455b7460
Jon Duesterhoeft <jon.duesterhoeft@gmail.com>
2023-06-24 12:42:23 -0500

    added insertion_sort

--- src/sort.rs
+++ src/sort.rs
@@ -1,5 +1,18 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn insertion_sort(data: &mut Vec<i32>, asc: bool) {
+    for i in 1..data.len() {
+        let mut j = i;
+        if asc {
+            while j > 0 && data[j - 1] > data[j] {
+                data.swap(j - 1, j);
+                j = j - 1;
+            }
+        } else {
+            while j > 0 && data[j - 1] < data[j] {
+                data.swap(j - 1, j);
+                j = j - 1;
+            }
+        }
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +20,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_insertion_sort_asc() {
+        let mut test_data = vec![-1, 5, 4, 1, 0];
+        insertion_sort(&mut test_data, true);
+        let expected = vec![-1, 0, 1, 4, 5];
+        assert_eq!(test_data, expected);
+    }
+
+    #[test]
+    fn test_insertion_sort_desc() {
+        let mut test_data = vec![-1, 5, 4, 1, 0];
+        insertion_sort(&mut test_data, false);
+        let expected = vec![5, 4, 1, 0, -1];
+        assert_eq!(test_data, expected);
     }
 }
